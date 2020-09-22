5e9d347f648f33ed6962f7d6aa862ec6a78c1743
Obanex <69535465+obanex@users.noreply.github.com>
2020-09-22 09:44:02 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+function currentTime() {
+	var date = new Date();
+	/* creation de la classe date */
+	var hour = date.getHours();
+	var min = date.getMinutes();
+	var sec = date.getSeconds();
+	var midday = "AM";
+	midday = hour >= 12 ? "PM" : "AM";
+	/* AM et PM */
+	hour = hour == 0 ? 12 : hour > 12 ? hour - 12 : hour;
+	/* format 12h */
+	hour = updateTime(hour);
+	min = updateTime(min);
+	sec = updateTime(sec);
+	document.getElementById("clock").innerText =
+		hour + " : " + min + " : " + sec + " " + midday;
+	/* heure */
+	var t = setTimeout(currentTime, 1000); /* timer */
+}
+
+function updateTime(k) {
+	/* 0 avant*/
+	if (k < 10) {
+		return "0" + k;
+	} else {
+		return k;
+	}
+}
+
+currentTime();
